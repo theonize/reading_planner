76d05bc7454c8e09af42fb0d88074ca5f5443ea2
fingerskier <fingerskier@gmail.com>
2024-10-12 12:32:21 -0500

    base fx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import './App.css';
+import {useEffect, useState} from 'react'
+import useBible from './hook/useBible'
+import useLocalStorage from './hook/useLocalStorage'
+
+import './App.css'
 
 
 export default function App() {
+  const {section, sectionSize} = useBible()
+  const [day, setDay] = useLocalStorage('day', 0)
+  const [numDays, setNumDays] = useLocalStorage('numDays', 90)
+  const [currentSection, setCurrentSection] = useState({})
+  const [versesPerDay, setVersesPerDay] = useState(0)
+  
+  
+  useEffect(() => {
+    if (numDays) {
+      setVersesPerDay(sectionSize(numDays))
+    }
+  }, [numDays])
+  
+  
+  useEffect(() => {
+    if (day && versesPerDay) {
+      setCurrentSection(section(day, versesPerDay))
+    }
+  }, [day, versesPerDay])
+  
+  
   return <>
     <h1>Reading Planner</h1>
-
+    
+    
+    <label htmlFor="day">Current Day:</label>
+    
+    <input type="number" id="day" name="day" 
+      onChange={e=>setDay(e.target.value)}
+      value={day}
+    />
+    
+    
     <label htmlFor="num-days">
       Number of days to completion:
     </label>
-
-    <input type="number" id="num-days" name="num-days" />
+    
+    <input type="number" id="num-days" name="num-days" 
+      max="1189"
+      onChange={e=>setNumDays(e.target.value)}
+      value={numDays}
+    />
+    
+    <div>
+      Verses per Day: {versesPerDay}
+    </div>
+    
+    <div>
+      <pre>{JSON.stringify(currentSection,null,2)}</pre>
+    </div>
   </>
 }
---
+++ src/hook/useBible.js
@@ -0,0 +1,71 @@
+import {useEffect, useState} from 'react'
+import NET from '../lib/net.json'
+
+
+export default function useBible(version='NET') {
+  const [queue, setQueue] = useState([])
+  const [totalVerses, setTotalVerses] = useState(0)
+  
+  
+  const section = (day, numVerses) => {
+    const startVerse = (day-1) * numVerses + 1
+    const endVerse = Math.min(day * numVerses, totalVerses)
+    
+    let chapterStart = 1
+    let end, start
+    
+    
+    console.log(day, numVerses, startVerse, endVerse)
+    for (let chapterInfo of queue) {
+      let [book,chapter,chapterLength] = chapterInfo.split('|')
+      
+      chapter = +chapter
+      chapterLength = +chapterLength
+      
+      const chapterEnd = chapterStart + chapterLength
+
+      console.log({book,chapter,chapterLength,chapterStart,chapterEnd})
+      
+      if (!start && (startVerse <= chapterEnd)) {
+        start = { book, chapter, verse: startVerse - chapterStart + 1 }
+      }
+      
+      if (!end && (endVerse <= chapterEnd)) {
+        end = { book, chapter, verse: endVerse - chapterStart + 1 }
+        break
+      }
+      
+      chapterStart += chapterLength
+    }
+    
+    
+    return { start, end }
+  }
+  
+  
+  const sectionSize = numDays => Math.ceil(totalVerses / numDays)
+  
+  
+  useEffect(() => {
+    const Q = []
+    let newTotal = 0
+    
+    for (let book in NET) {
+      for (let chapter in NET[book]) {
+        const chapterLength = NET[book][chapter].length
+        Q.push(`${book}|${chapter}|${chapterLength}`)
+        newTotal += chapterLength
+      }
+    }
+    
+    setTotalVerses(newTotal)
+    
+    setQueue(Q)
+  }, [version])
+  
+  
+  return {
+    section,
+    sectionSize,
+  }
+}
